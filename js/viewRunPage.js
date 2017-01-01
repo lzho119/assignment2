2dd680a1058e0aef970298b96d355dc545001d9f
deweipan <dpan40@student.monash.edu>
2017-01-01 19:33:22 +0800

    edit js of viewRun page

--- js/viewRunPage.js
+++ js/viewRunPage.js
@@ -2,13 +2,52 @@
 
 // The following is sample code to demonstrate navigation.
 // You need not use it for final app.
+var viewStart;
+var viewArrival;
+var itemIndex;
 
-var runIndex = localStorage.getItem(APP_PREFIX + "-selectedRun"); 
-if (runIndex !== null)
-{
-    // If a run index was specified, show name in header bar title. This
-    // is just to demonstrate navigation.  You should set the page header bar
-    // title to an appropriate description of the run being displayed.
-    var runNames = [ "Run A", "Run B" ];
-    document.getElementById("headerBarTitle").textContent = runNames[runIndex];
+function initViewRun(){
+	itemIndex = JSON.parse(localStorage.getItem("index"));
+	viewRun();
+}
+function viewRun () {
+	run = JSON.parse(localStorage.getItem(APP_PREFIX + "-RunList"))[itemIndex];
+	var startLat = run.start_lat;
+	var startLon = run.start_lon;
+	var arrivalLat = run.arrival_lat;
+	var arrivalLon = run.arrival_lon;
+	var beginTime = run.beginTime;
+	var endTime = run.endTime;
+	var startLatlon=new google.maps.LatLng(startLat, startLon)
+	var mapholder=document.getElementById('mapholder')
+	mapholder.style.height='250px';
+	mapholder.style.width='500px';
+	var myOptions={
+	center:startLatlon,zoom:14,
+	mapTypeId:google.maps.MapTypeId.ROADMAP,
+	mapTypeControl:false,
+	navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+	};
+	arrivalLatlon=new google.maps.LatLng(arrivalLat, arrivalLon)
+	map=new google.maps.Map(document.getElementById("mapholder"),myOptions);
+	viewStart=new google.maps.Marker({position:startLatlon,map:map,title:"Your start!"});
+	viewArrival = new google.maps.Marker({position:arrivalLatlon,map:map,title:"Your arrival!"});
+	traveled = google.maps.geometry.spherical.computeDistanceBetween(startLatlon, arrivalLatlon);
+	time = (endTime - beginTime)/1000;
+	average = traveled / time;
+
+	document.getElementById('traveled').value = traveled;
+	document.getElementById('time').value = time;
+	document.getElementById('average').value = average;
+}
+
+function deleteRun(){
+	var run = localStorage.getItem(APP_PREFIX + "-RunList");
+	run.splice(itemIndex, itemIndex);
+	window.location.href="index.html";
+} 
+
+function restart(){
+	window.location.href="newRun.html";
+	localStorage.setItem("status", "retry");
 }
